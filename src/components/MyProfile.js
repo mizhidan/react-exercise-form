6aa81cd7a97a8db05a46bbd39d13a2dc76bfbe59
mizhidan <dj9824@126.com>
2020-08-12 21:36:02 +0800

    [danjie] feat: complete form except css

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,9 +2,68 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+    state = {
+      name: '',
+      Gender: 'Male',
+      Description: '',
+      isChecked: false
+    }
+
+  handleNameChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleGenderChange = event => {
+    this.setState({
+      Gender: event.target.value
+    })
+  }
+
+  handleDescriptionChange = event => {
+    this.setState({
+      Description: event.target.value
+  })
+}
+
+handleCheckboxChange = event => {
+  this.setState({
+    isChecked: event.target.checked
+})
+}
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.name, this.state.Gender, this.state.Description);
+  }
+
   render() {
     return (
-      <form></form>
+      <form>
+        <h1>My Profile</h1>
+        <h2>Name</h2>
+        <input 
+        type="text" 
+        id="name"  
+        value={this.state.name}
+        onChange={this.handleNameChange} />
+        <h2>Gender</h2>
+        <select id="gender" value={this.state.value} onChange={this.handleGenderChange}>
+          <option value="Male" onSelect={this.handleGenderChange}>Male</option>
+          <option value="Female" onSelect={this.handleGenderChange}>Female</option>
+        </select>
+        <h2>Descripion</h2>
+        <textarea defaultValue="Description about yourself" value={this.state.value} onChange={this.handleDescriptionChange}></textarea>
+        <div>
+        <input type="checkbox" checked={this.state.value} onClick={this.handleCheckboxChange}></input>
+        <span>I have read the terms of conduct</span>
+        </div>
+        <button 
+        type="submit" 
+        disabled={!this.state.name || !this.state.Description || !this.state.isChecked}
+        onClick={this.handleSubmit}>Submit</button>
+      </form>
     );
   }
 }
